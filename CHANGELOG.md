9bbb7176d0bf71eceed9d5f17ecd1ffd092fe58d
Valentin Knabel <dev@vknabel.com>
2018-11-20 16:53:35 +0100

    Added missing SwiftPM product

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## 0.1.2
+
+* Missing SwiftPM product
+
 ## 0.1.1
 
 * Bumped Mint to 0.10.1
--- Package.swift
+++ Package.swift
@@ -5,16 +5,23 @@ import PackageDescription
 
 let package = Package(
     name: "MintArrow",
+    products: [
+        .executable(
+            name: "MintArrow",
+            targets: ["MintArrow"]
+        ),
+    ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/vknabel/ArrowKit.git", .branch("master")),
-        .package(url: "https://github.com/yonaskolb/Mint.git", from: "0.10.1"),
+        .package(url: "https://github.com/yonaskolb/Mint.git", .exact("0.10.1")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "MintArrow",
-            dependencies: ["MintKit", "ArrowKit"]),
+            dependencies: ["MintKit", "ArrowKit"]
+        ),
     ]
 )
